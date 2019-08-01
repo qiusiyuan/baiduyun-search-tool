113bffabc21c3c43002763097381534068490fcc
Siyuan Qiu <siyuan.qiu@mail.utoronto.ca>
2019-07-31 23:28:35 -0400

    ocr

--- README.md
+++ README.md
@@ -4,3 +4,19 @@ This is a search tool to search baiduyun source
 # how to use
 python3 interface.py
 and input prompt input request
+
+# dependency
+```
+#install tesseract mac
+brew install tesseract --all-language
+
+#download chinese data
+https://github.com/tesseract-ocr/tessdata/raw/master/chi_sim.traineddata
+
+#install python libs
+pip3 install pytesseract
+pip3 install Pillow
+
+#install selenium
+https://selenium-python.readthedocs.io/
+```
--- urlChecker.py
+++ urlChecker.py
@@ -2,10 +2,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import pytesseract
+from PIL import Image
+from io import BytesIO
 import json
 import logging
 import random
 import string
+
+invalidText = [
+  "该 分 享 文 件 已 过 期",
+  "此 链 接 分 享 内 容 可 能 因 为 涉 及 侵 权"
+]
 
 def selenium_check_validation(pan_url):
   logging.warning("validating " + pan_url + "...")
@@ -18,14 +26,21 @@ def selenium_check_validation(pan_url):
     driver.quit()
     return "invalid"
   else:
-    logging.warning("链接有效")
-    ans = input("continue?")
-    if ans == "":
-      driver.quit()
+    png = driver.get_screenshot_as_png()
+    driver.quit()
+    img = Image.open(BytesIO(png))
+    text = pytesseract.image_to_string((img), lang='chi_sim')
+    if not validate_text(text):
       return "continue"
-    else:
-      return "stop"
+    logging.warning("链接有效")
+    if "请 输 入 提 取 码" in text:
+      return "needpassword"
+    return "stop"
   
+def validate_text(text):
+  if any(map(lambda invalid: invalid in text, invalidText)):
+    return False
+  return True
 
 if __name__=="__main__":
   data = json.load(open('baidu_raw.json'))
@@ -36,9 +51,16 @@ if __name__=="__main__":
     for link in links:
       pan_url = link['link']
       validation = selenium_check_validation(pan_url)
+      # nice
       if validation == "stop":
         new_dict["page"] = host_url
         new_dict["url"] = link['link']
         with open("baidu_raw" + ''.join(random.choice(string.digits) for _ in range(4)) + ".json", "w") as fd:
           json.dump(new_dict,fd)
-          break
+      # need password
+      elif validation == "needpassword":
+        new_dict["page"] = host_url
+        new_dict["url"] = link['link']
+        new_dict["mode"] = "提取码"
+        with open("baidu_raw" + ''.join(random.choice(string.digits) for _ in range(4)) + ".json", "w") as fd:
+          json.dump(new_dict,fd)
